6043d515a2339f7d5abe0f457d340f47544e2236
jerrynim <tjerry3@naver.com>
2018-12-24 14:04:13 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
