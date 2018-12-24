166e9066f05bd90af2fc1c871af47118a36e3c25
jerrynim <tjerry3@naver.com>
2018-12-24 15:25:04 +0900

    apollo init

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { ApolloProvider } from "react-apollo";
 import ReactDOM from "react-dom";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+import client from "./apollo";
+import App from "./Components/App";
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root") as HTMLElement
+);
