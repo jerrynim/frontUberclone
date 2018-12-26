853c61a598550c429427d556ad6b8749dc9a8e14
jerrynim <tjerry3@naver.com>
2018-12-26 16:42:14 +0900

    eventHandler with typescript

---
+++ src/Routes/PhoneLogin/PhoneLoginContainer.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { RouteComponentProps } from "react-router-dom";
+import PhoneLoginPresenter from "./PhoneLoginPresenter";
+
+interface IState {
+  countryCode: string;
+  phoneNumber: string;
+}
+
+class PhoneLoginContainer extends React.Component<
+  RouteComponentProps<any>,
+  IState
+> {
+  public state = {
+    countryCode: "+82",
+    phoneNumber: "12345"
+  };
+  public render() {
+    const { countryCode, phoneNumber } = this.state;
+    return (
+      <PhoneLoginPresenter
+        countryCode={countryCode}
+        phoneNumber={phoneNumber}
+        onInputChange={this.onInputChange}
+        onSubmit={this.onSubmit}
+      />
+    );
+  }
+  public onInputChange: React.ChangeEventHandler<
+    HTMLInputElement | HTMLSelectElement
+  > = (event) => {
+    const {
+      target: { name, value }
+    } = event;
+    this.setState({
+      [name]: value
+    } as any);
+  };
+  public onSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+  };
+}
+
+export default PhoneLoginContainer;
