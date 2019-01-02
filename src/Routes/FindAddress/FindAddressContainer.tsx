4511d8d5ff3bfbdfb2f593ed8c2ee3f826e8e50a
jerrynim <tjerry3@naver.com>
2019-01-02 14:25:06 +0900

    get mapCenter

--- src/Routes/FindAddress/FindAddressContainer.tsx
+++ src/Routes/FindAddress/FindAddressContainer.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDom from "react-dom";
 import FindAddressPresenter from "./FindAddressPresenter";
 
-class FindAddressContainer extends React.Component<any> {
+interface IState {
+  lat: number;
+  lng: number;
+}
+
+class FindAddressContainer extends React.Component<any, IState> {
   public mapRef: any;
   public map: google.maps.Map;
   constructor(props) {
@@ -28,6 +33,10 @@ class FindAddressContainer extends React.Component<any> {
     const {
       coords: { latitude, longitude }
     } = position;
+    this.setState({
+      lat: latitude,
+      lng: longitude
+    });
     this.loadMap(latitude, longitude);
   };
 
@@ -44,6 +53,16 @@ class FindAddressContainer extends React.Component<any> {
       zoom: 11
     };
     this.map = new maps.Map(mapNode, mapConfig);
+    this.map.addListener("dragend", this.handleDragEnd);
+  };
+  public handleDragEnd = () => {
+    const newCenter = this.map.getCenter();
+    const lat = newCenter.lat();
+    const lng = newCenter.lng();
+    this.setState({
+      lat,
+      lng
+    });
   };
 }
 
